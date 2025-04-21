9daa44b34a9a3ec625c3eceb94445207ce0f681c
Dennis Byberg <dennis.byberg@hotmail.com>
2025-04-22 00:04:07 +0200

    Implement generic repository pattern

---
+++ src/MerchStore.Infrastructure/Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,11 @@
+using MerchStore.Domain.Entities;
+using MerchStore.Domain.Interfaces;
+
+namespace MerchStore.Infrastructure.Persistence.Repositories;
+
+public class ProductRepository : Repository<Product, Guid>, IProductRepository
+{
+    public ProductRepository(AppDbContext context) : base(context)
+    {
+    }
+}
