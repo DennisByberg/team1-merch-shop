dc4f07e7f7a832446c5e1a02c52930a72a6cc14f
Dennis Byberg <dennis.byberg@hotmail.com>
2025-05-07 16:17:28 +0200

    enhance the Function with OpenAPI Attributes

--- infra/ReviewApiFunction/Program.cs
+++ infra/ReviewApiFunction/Program.cs
@@ -1,9 +1,18 @@
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using ReviewApiFunction;
+using Microsoft.Extensions.DependencyInjection; // Make sure to include your namespace
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+
+// Add OpenAPI configuration using our custom classes
+builder.Services.AddSingleton<IOpenApiConfigurationOptions, SwaggerConfiguration>();
+
+// Add custom UI options
+builder.Services.AddSingleton<IOpenApiCustomUIOptions, SwaggerUIConfiguration>();
 
 // Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
 // builder.Services
