57f550370a0207b126aa96dd0da0121436b44a14
Dennis Byberg <dennis.byberg@hotmail.com>
2025-05-08 11:17:23 +0200

    implementing the Infrastructure Layer for the External Review Service

--- src/MerchStore.Infrastructure/DependencyInjection.cs
+++ src/MerchStore.Infrastructure/DependencyInjection.cs
@@ -5,6 +5,8 @@ using MerchStore.Application.Common.Interfaces;
 using MerchStore.Domain.Interfaces;
 using MerchStore.Infrastructure.Persistence;
 using MerchStore.Infrastructure.Persistence.Repositories;
+using MerchStore.Infrastructure.ExternalServices.Reviews.Configurations;
+using MerchStore.Infrastructure.ExternalServices.Reviews;
 
 namespace MerchStore.Infrastructure;
 
@@ -13,7 +15,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<AppDbContext>(opt => 
+        services.AddDbContext<AppDbContext>(opt =>
             opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
 
@@ -23,9 +25,23 @@ public static class DependencyInjection
         services.AddLogging();
         services.AddScoped<AppDbContextSeeder>();
 
+        // Register the external review services
+        services.AddReviewServices(configuration);
+
         return services;
     }
 
+    // This method is responsible for registering the external review services.
+    public static IServiceCollection AddReviewServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<ReviewApiOptions>(configuration.GetSection(ReviewApiOptions.SectionName));
+        services.AddHttpClient<ReviewApiClient>().SetHandlerLifetime(TimeSpan.FromMinutes(5));
+        services.AddSingleton<MockReviewService>();
+        services.AddScoped<IReviewRepository, ExternalReviewRepository>();
+        return services;
+    }
+
+    // This method is responsible for seeding the database with initial data.
     public static async Task SeedDatabaseAsync(this IServiceProvider serviceProvider)
     {
         using var scope = serviceProvider.CreateScope();
